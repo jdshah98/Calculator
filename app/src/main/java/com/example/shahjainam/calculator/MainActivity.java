0283ff5c19e1713dd45f2bad3176d2d154a46e51
jainam <jdshahstudio@gmail.com>
2019-07-14 11:57:24 +0530

    Expression View added

--- app/src/main/java/com/example/shahjainam/calculator/MainActivity.java
+++ app/src/main/java/com/example/shahjainam/calculator/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.shahjainam.calculator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
